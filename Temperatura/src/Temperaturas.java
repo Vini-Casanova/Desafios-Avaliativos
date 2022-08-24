d1862d2f988a980cd359c40533d32c1bce0cc584
Vinicius Santos <dukeofgod@outlook.com>
2022-08-23 21:27:17 -0300

    Ajustes finais

--- Temperatura/src/Temperaturas.java
+++ Temperatura/src/Temperaturas.java
@@ -1,19 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
-
-/*
- De graus Celsius (C) para graus Fahrenheit (F): F = (9 × C/5) + 32)
- • De graus Celsius (C) para graus Kelvin (K): K = C + 273.15
- • De graus Celsius (C) para graus Réaumur (Re): Re = C ∗ 4/5
-• De graus Fahrenheit (F) para graus Celsius (C): C = (F − 32) × +5/9
-• De graus Kelvin (K) para graus Celsius (C): C = K − 273.15
-• De graus Kelvin (K) para graus Rankine (R): R = K ∗ 1.8
-• De graus Réaumur (Re) para graus Celsius (C): C = Re ∗ 5/4
-• De graus Rankine (R) para graus Kelvin (K): K = R/1.8
- */
-
-public class Temperaturas {
-
+ class Temperaturas {
   private double celsius;
   private double fahrenheit;
   private double kelvin;
